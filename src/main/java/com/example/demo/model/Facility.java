52365f2dd3506b5e39d63ae40c744f452da4fac5
bala-amypo <bala@amypo.in>
2025-12-21 17:31:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Facility.java
+++ src/main/java/com/example/demo/model/Facility.java
@@ -1,4 +1,26 @@
-package com.example.demo.entity;
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "facilities")
 public class Facility{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    private String description;
     
+    @Column(nullable=false)
+    private String openTime;
+
+    @Column(nullable=false)
+    private String closeTime;  
 }
