89b3139ba5c27a17951a17ff8da9c413e56db166
bala-amypo <bala@amypo.in>
2025-12-26 17:42:07 +0000

    User commit

--- src/main/java/com/example/demo/model/Facility.java
+++ src/main/java/com/example/demo/model/Facility.java
@@ -1,38 +1,12 @@
-package com.example.demo.model;
+package com.example.demo.service;
 
-import jakarta.persistence.*;
-import java.time.LocalTime;
+import com.example.demo.model.Facility;
+import java.util.List;
 
-@Entity
-@Table(name = "facilities")
-public class Facility {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-    private LocalTime openTime;
-    private LocalTime closeTime;
-
-    public Facility() {}
-
-    public Facility(String name, LocalTime openTime, LocalTime closeTime) {
-        this.name = name;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
-    }
-
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public LocalTime getOpenTime() { return openTime; }
-    public void setOpenTime(LocalTime openTime) { this.openTime = openTime; }
-
-    public LocalTime getCloseTime() { return closeTime; }
-    public void setCloseTime(LocalTime closeTime) { this.closeTime = closeTime; }
+public interface FacilityService {
+    Facility createFacility(Facility facility);
+    Facility getFacilityById(Long id);
+    List<Facility> getAllFacilities();
+    Facility updateFacility(Long id, Facility facility);
+    void deleteFacility(Long id);
 }
