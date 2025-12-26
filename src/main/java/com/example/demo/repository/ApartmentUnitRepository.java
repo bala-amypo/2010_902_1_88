4059c89b0ec81c8638f83fd7da9106ea5326ec2d
bala-amypo <bala@amypo.in>
2025-12-26 15:31:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApartmentUnitRepository.java
+++ src/main/java/com/example/demo/repository/ApartmentUnitRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.ApartmentUnit;
-import com.example.demo.model.User;
+import com.example.demo.entity.ApartmentUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
+@Repository
 public interface ApartmentUnitRepository extends JpaRepository<ApartmentUnit, Long> {
-    Optional<ApartmentUnit> findByOwner(User user);
+    List<ApartmentUnit> findByBuilding(String building);
+    List<ApartmentUnit> findByFloor(Integer floor);
 }
--- src/main/java/com/example/demo/repository/FacilityRepository.java
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -1,10 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Facility;
+import com.example.demo.entity.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
-    Optional<Facility> findByName(String name);
 }
