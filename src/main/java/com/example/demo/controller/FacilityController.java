12185365573e9c271b08d589675e994e521ac550
bala-amypo <bala@amypo.in>
2025-12-26 17:51:07 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityController.java
+++ src/main/java/com/example/demo/controller/FacilityController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Facility;
 import com.example.demo.service.FacilityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,16 +12,45 @@ import java.util.List;
 @RequestMapping("/facilities")
 public class FacilityController {
 
-    @Autowired
-    private FacilityService facilityService;
+    private final FacilityService facilityService;
 
-    @PostMapping
-    public Facility addFacility(@RequestBody Facility facility) {
-        return facilityService.addFacility(facility);
+    @Autowired
+    public FacilityController(FacilityService facilityService) {
+        this.facilityService = facilityService;
     }
 
+    // Create a new facility
+    @PostMapping
+    public ResponseEntity<Facility> createFacility(@RequestBody Facility facility) {
+        Facility createdFacility = facilityService.createFacility(facility);
+        return ResponseEntity.ok(createdFacility);
+    }
+
+    // Get all facilities
     @GetMapping
-    public List<Facility> getFacilities() {
-        return facilityService.getAllFacilities();
+    public ResponseEntity<List<Facility>> getAllFacilities() {
+        List<Facility> facilities = facilityService.getAllFacilities();
+        return ResponseEntity.ok(facilities);
+    }
+
+    // Get facility by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Facility> getFacilityById(@PathVariable Long id) {
+        Facility facility = facilityService.getFacilityById(id);
+        return ResponseEntity.ok(facility);
+    }
+
+    // Update a facility
+    @PutMapping("/{id}")
+    public ResponseEntity<Facility> updateFacility(@PathVariable Long id, @RequestBody Facility facility) {
+        Facility updatedFacility = facilityService.updateFacility(id, facility);
+        return ResponseEntity.ok(updatedFacility);
+    }
+
+    // Delete a facility
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFacility(@PathVariable Long id) {
+        facilityService.deleteFacility(id);
+        return ResponseEntity.noContent().build();
     }
 }
